485a94d1c6bf5c654d8a45be965d6eed40cb037c
Vinit Shah <64981658+vinitshah0110@users.noreply.github.com>
2020-06-14 14:31:46 +0530

    Update json-numberscrap.py

--- Using Python to Access Web Data/JSON/json-numberscrap.py
+++ Using Python to Access Web Data/JSON/json-numberscrap.py
@@ -1,8 +1,14 @@
+'''
+The program will prompt for a URL, read the JSON data from that URL using urllib and then parse and extract the comment counts from the
+JSON data, compute the sum of the numbers in the file and enter the sum below: We provide two files for this assignment.
+
+Sample data: http://py4e-data.dr-chuck.net/comments_42.json (Sum=2553)
+Actual data: http://py4e-data.dr-chuck.net/comments_618666.json (Sum ends with 28)
+'''
+
 import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
-
-
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
